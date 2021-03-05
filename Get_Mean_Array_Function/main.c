3244aedf3a28eb762b351398a0e2016b1d9bde2a
Canberk Sahin <39211643+CannberkSahin@users.noreply.github.com>
2021-03-05 18:37:03 +0300

    Add files via upload

---
+++ Get_Mean_Array_Function/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define    SIZE    100
+
+void randomize(void)
+{
+    srand((unsigned int)time(0));
+}
+
+void set_random_array(int *p, size_t size)
+{
+    for(int i = 0; i < size; ++i){
+        p[i] = rand() % 1000;
+    }
+}
+
+void display_array(const int *p, size_t size)
+{
+    for(int i = 0; i < size; ++i){
+        if(i && i % 20 == 0)
+            printf("\n");
+        printf("%3d ", p[i]);
+    }
+}
+
+
+int sum_array(const int *p, size_t size)
+{
+    int sum = 0;
+
+    for(int i = 0; i < size; ++i){
+        sum += p[i];
+    }
+
+    return sum;
+}
+
+double get_mean_array(const int *p, size_t size)
+{
+    return (double)sum_array(p, size) / size;
+}
+
+int main()
+{
+    int a[SIZE];
+
+    randomize();
+    set_random_array(a, SIZE);
+    display_array(a, SIZE);
+    printf("\n\nSum of the array elements = %d\n", sum_array(a, SIZE));
+    printf("\nMean of the array = %f\n",get_mean_array(a,SIZE));
+}
