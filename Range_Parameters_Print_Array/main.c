47b7149e0cf8ac5762901ddec8b9b890ab04d102
Canberk Sahin <39211643+CannberkSahin@users.noreply.github.com>
2021-03-08 13:02:04 +0300

    Add files via upload

--- Range_Parameters_Print_Array/main.c
+++ Range_Parameters_Print_Array/main.c
@@ -11,5 +11,5 @@ void print_array(const int *pf, const int *pe)
 int main()
 {
     int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    print_array(a, a+10); //Invoke unction first and last elements of the array
+    print_array(a, a+10); //Invoke function first and last elements of the array
 }
