9dbceaf92fbb6b57e4ca53628f2b998af11e6257
Canberk Sahin <39211643+CannberkSahin@users.noreply.github.com>
2021-03-08 14:10:30 +0300

    Add files via upload

--- Range_Parameters_Print_Array/main.c
+++ Range_Parameters_Print_Array/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*First Method*/
+void print_array_comparison(const int *pf, const int *pe)
+{
+    while(pf < pe)
+        printf("%3d ", *pf++);
+    printf("\n");
+}
+
+/*Second Method*/
 void print_array(const int *pf, const int *pe)
 {
     while(pf != pe)
@@ -11,5 +20,7 @@ void print_array(const int *pf, const int *pe)
 int main()
 {
     int a[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    print_array(a, a+10); //Invoke function first and last elements of the array
+    print_array(a, a + 10); //Invoke function first and last elements of the array
+
+    print_array_comparison(a, a + 10); //Invoke function first and last elements of the array
 }
