2538648e7c2352c5929c1e26d6cce64abe9fe6d0
Canberk Sahin <39211643+CannberkSahin@users.noreply.github.com>
2021-03-08 21:40:16 +0300

    Add files via upload

---
+++ Swap_Array_Min_To_Max_Elements/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mutility.h"
+
+//Define Macros
+#define      SIZE     20
+
+int *get_array_min(const int *p, size_t size)
+{
+    const int *pmin = p;
+
+    for(size_t i=1; i < size; ++i){
+        if(p[i]<*pmin)
+            pmin = p + i;
+    }
+
+    return (int *)pmin; //const cast
+}
+
+int *get_array_max(const int *p, size_t size)
+{
+    const int *pmax = p;
+
+    for(size_t i=1; i < size; ++i){
+        if(p[i]>*pmax)
+            pmax = p + i;
+    }
+
+    return (int *)pmax; //const cast
+}
+
+int main()
+{
+    int a[SIZE];
+    int *pmax, *pmin;
+
+    randomize();
+    set_random_array(a, SIZE);
+    display_array(a, SIZE);
+    //Call swap function
+    swap(get_array_min(a, SIZE), get_array_max(a, SIZE));
+    display_array(a, SIZE);
+}
