1499d7134c5373041cffa7f7c49441edc04ce873
Canberk Sahin <39211643+CannberkSahin@users.noreply.github.com>
2021-03-17 10:31:21 +0300

    Add files via upload

--- Char_Pointer_Array/main.c
+++ Char_Pointer_Array/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define    asize(x)      (sizeof(x) / sizeof(x[0]))
+
+void randomize(void)
+{
+    srand((unsigned int)time(NULL));
+}
 
 int main()
 {
     //Look-up table
-    char *pmons[] = {
+    const char *const pmons[] = {
+    "", //null string literal
     "January",
     "February",
     "March",
@@ -19,6 +28,21 @@ int main()
     "December"
     };
 
+
     for(int i=0; i< 12; ++i)
         puts(pmons[i]); //Puts gives the new line character to the standard output stream
+
+    int n;
+    printf("\nEnter a number between 1 and 12:");
+    scanf("%d",&n);
+
+    printf("%d month of the year is %s\n",n, pmons[n]);
+
+    printf("Press the enter key:\n");
+    randomize();
+
+    while(1){
+        printf("%s ",pmons[rand() % asize(pmons)]);
+        getchar();
+    }
 }
